7708d38774a86b31dae320ec4981168828071cea
404Setup <153366651+404Setup@users.noreply.github.com>
2026-07-13 09:37:11 +0800

    fix: modmenuscreen

--- src/main/java/one/pkg/kreno_fpatcher/ModMenuScreen.java
+++ src/main/java/one/pkg/kreno_fpatcher/ModMenuScreen.java
@@ -1,23 +1,31 @@
 package one.pkg.kreno_fpatcher;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import net.minecraft.client.gui.screens.Screen;
 import one.pkg.libsl.api.ui.seeui.SeeUIBuilder;
 
-public class ModMenuScreen implements ConfigScreenFactory<Screen> {
+public class ModMenuScreen implements ModMenuApi {
     @Override
-    public Screen create(Screen screen) {
-        return SeeUIBuilder.builder()
-                .clazz(ModConfig.class)
-                .lastScreen(screen)
-                .useOreUI(ModConfig.GUI.isOreUI())
-                .onSaved(() -> {
-                    try {
-                        ModConfig.config.saveAllConfigurations();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                })
-                .build();
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return new Factory();
+    }
+
+    public static class Factory implements ConfigScreenFactory<Screen> {
+
+        @Override
+        public Screen create(Screen screen) {
+            return SeeUIBuilder.builder()
+                    .clazz(ModConfig.class)
+                    .lastScreen(screen)
+                    .useOreUI(ModConfig.GUI.isOreUI())
+                    .onSaved(() -> {
+                        try {
+                            ModConfig.config.saveAllConfigurations();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }).build();
+        }
     }
 }
